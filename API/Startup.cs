099cd74b6e9384fe92a32f974a4b3437aa17f572
zakariabijoy <zakaria.bijoy@live.com>
2021-06-18 13:43:18 +0600

    add a dbcontext class for entity framwork core -Api

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,7 +28,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite("Connection String");
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
