1bbab5057b6f49e0dfc8211606e9a7a34e6b6cdd
zakariabijoy <zakaria.bijoy@live.com>
2021-06-20 00:33:53 +0600

    displaying th fetched users in the browser -client and some bug fix -Api'

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,6 +3,6 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
-        public int UserName { get; set; }
+        public string UserName { get; set; }
     }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.getUsers();
+    
   }
 
   getUsers() {
