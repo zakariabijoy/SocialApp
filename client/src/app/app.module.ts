539ef33dba2b57f81dae18b6e2f28f3908ae92f1
zakariabijoy <zakaria.bijoy@live.com>
2021-06-20 17:09:17 +0600

    using https in angular  -client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
