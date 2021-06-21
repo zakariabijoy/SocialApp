7b2d30114aba4bf7297541623194c29f3f4bd101
zakariabijoy <zakaria.bijoy@live.com>
2021-06-21 16:07:23 +0600

    adding a token service -api

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -43,6 +45,7 @@ namespace API
             });
 
             services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
