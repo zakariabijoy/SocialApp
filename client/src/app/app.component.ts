fe0633abe9ea26bfaf88664a5de70c6d2f278f04
zakariabijoy <zakaria.bijoy@live.com>
2021-06-23 17:18:12 +0600

    parent to  child communiation --client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from 'src/_services/account.service';
@@ -12,10 +11,9 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient, private accountService : AccountService) {}
+  constructor(private accountService : AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
     
   }
@@ -25,14 +23,5 @@ export class AppComponent implements OnInit {
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      (response) => {
-        this.users = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+  
 }
