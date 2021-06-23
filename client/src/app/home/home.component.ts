818b0f5dad151b1c691d3c59a4dd494b6426c18b
zakariabijoy <zakaria.bijoy@live.com>
2021-06-24 01:03:40 +0600

    hooking up the register method to the service -client

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -8,21 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode =false;
-  users:any;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getUsers();
+   
   }
 
   registerToggle(){
     this.registerMode = !this.registerMode
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
-  }
+
 
   cancelRegisterModel(event: boolean){
     this.registerMode = event;
