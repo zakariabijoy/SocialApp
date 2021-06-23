d521171ef5b5e0c4a4bbca68e75948efefe7ae2f
zakariabijoy <zakaria.bijoy@live.com>
2021-06-23 17:28:33 +0600

    child to parent communication --client

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -24,4 +24,8 @@ export class HomeComponent implements OnInit {
     this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
   }
 
+  cancelRegisterModel(event: boolean){
+    this.registerMode = event;
+  }
+
 }
