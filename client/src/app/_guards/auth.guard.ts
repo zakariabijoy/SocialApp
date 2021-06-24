c6785782c05a34ecd12e86c416cf41cca8cfcd6f
zakariabijoy <zakaria.bijoy@live.com>
2021-06-25 01:39:12 +0600

    bug fix of route guard --client

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -24,6 +24,7 @@ export class AuthGuard implements CanActivate {
       map((user) => {
         if (user) return true;
         this.toastr.error('You shall not pass!');
+        return false;
       })
     );
   }
