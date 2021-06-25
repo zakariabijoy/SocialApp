0ec82cd21dff4ae3f407eeaf9bb4087cb632eaf2
zakariabijoy <zakaria.bijoy@live.com>
2021-06-25 13:26:52 +0600

    adding a dummy route for grouping some route with auth guard (canActive) --client

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,17 +8,25 @@ import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
- {path:'', component: HomeComponent},
- {path:'members', component: MemberListComponent, canActivate: [AuthGuard]},
- {path:'members/:id', component: MemberDetailComponent},
- {path:'lists', component: ListsComponent},
- {path:'messages', component: MessagesComponent},
- {path:'**', component: HomeComponent, pathMatch:'full'},
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+    ],
+  },
+
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  
-exports: [RouterModule]
+
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
