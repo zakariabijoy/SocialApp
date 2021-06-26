4d74a8ac96f305f5da937a67bb61556eac853048
zakariabijoy <zakaria.bijoy@live.com>
2021-06-27 00:56:50 +0600

    handling server errors -api

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,3 +1,4 @@
+using System;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -34,12 +35,21 @@ namespace API.Controllers
         [HttpGet("server-error")]
         public ActionResult<string>GetServerError()
         {
-            var thing  = _context.Users.Find(-1);
+            try
+            {
+                 var thing  = _context.Users.Find(-1);
 
             var thingToReturn = thing.ToString();
 
             return thingToReturn;
 
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "computer says no!");
+            }
+           
         }
 
         
