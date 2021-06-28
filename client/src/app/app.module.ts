22ad039d7ff1aa84a5864a420a5ab7db044aefcf
zakariabijoy <zakaria.bijoy@live.com>
2021-06-28 13:07:39 +0600

    handling not found --client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
     ListsComponent,
     MessagesComponent,
     TestErrorsComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
