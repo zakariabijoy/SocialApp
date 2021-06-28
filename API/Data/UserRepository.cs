96743f58a45dff639fb3ef45bcea4339d7373858
zakariabijoy <zakaria.bijoy@live.com>
2021-06-28 17:17:58 +0600

    creating a repository for user -api

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUserNameAsync(string userName)
+        {
+             return await _context.Users.SingleOrDefaultAsync(u => u.UserName == userName);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+           return await _context.SaveChangesAsync()  > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,6 +18,8 @@ namespace API.Extensions
 
              services.AddScoped<ITokenService, TokenService>();
 
+             services.AddScoped<IUserRepository, UserRepository>();
+
              return services;
         }
     }
