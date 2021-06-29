1b282ac0e24ac2d9103f9145cc6e35d794814f73
zakariabijoy <zakaria.bijoy@live.com>
2021-06-30 02:00:48 +0600

    adding a photo gallery --client

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
@@ -13,7 +15,15 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
       positionClass: 'toast-bottom-right',
     }),
     TabsModule.forRoot(),
+    HttpClientModule,
+    NgxGalleryModule,
   ],
-  exports: [BsDropdownModule, ToastrModule, TabsModule],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    TabsModule,
+    HttpClientModule,
+    NgxGalleryModule,
+  ],
 })
 export class SharedModule {}
