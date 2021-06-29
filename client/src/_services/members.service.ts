baef70aa64fe81f6f32338ae4edb20c9f06fef52
zakariabijoy <zakaria.bijoy@live.com>
2021-06-29 17:53:03 +0600

    using an interceptor to send the jwt token --client

--- client/src/_services/members.service.ts
+++ client/src/_services/members.service.ts
@@ -1,29 +1,21 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { environment } from 'src/environments/environment';
 
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization : 'Bearer '+ JSON.parse(localStorage.getItem('user'))?.token
-  })
-}
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-
-  getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users',httpOptions);
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
-  getMember(userName : string){
-    return this.http.get<Member>(this.baseUrl + 'users/' + userName, httpOptions);
+  getMember(userName: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + userName);
   }
 }
