8c1982bdc3aa5d4d4703e600bec5e4fd6dfe110b
zakariabijoy <zakaria.bijoy@live.com>
2021-06-30 14:45:00 +0600

    persists the changes in api / user update functionality is added to api  --api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -45,5 +46,21 @@ namespace API.Controllers
             return await _userRepository.GetMemberAsync(userName);
             
         }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var user = await _userRepository.GetUserByUserNameAsync(userName);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        } 
     }
 }
