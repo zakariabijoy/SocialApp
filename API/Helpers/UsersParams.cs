9a1793015ec5137c51cc40bb6fa1459509bb72b7
zakariabijoy <zakaria.bijoy@live.com>
2021-07-04 23:26:41 +0600

    adding helper class for pagination -api

---
+++ API/Helpers/UsersParams.cs
@@ -0,0 +1,17 @@
+namespace API.Helpers
+{
+    public class UsersParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+
+        public int pageSize{
+            get => _pageSize;
+            set => _pageSize = (value >MaxPageSize)? MaxPageSize : value;
+        }
+
+        
+        
+    }
+}
