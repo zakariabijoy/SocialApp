bf7092ba240f7c6be13df4335a461641931f3e20
zakariabijoy <zakaria.bijoy@live.com>
2022-05-13 16:47:30 +0600

    refactoring chaching for member details --client

--- client/src/_services/members.service.ts
+++ client/src/_services/members.service.ts
@@ -45,8 +45,13 @@ export class MembersService {
   }
 
   getMember(userName: string) {
-    const member = this.members.find((x) => x.userName === userName);
-    if (member !== undefined) return of(member);
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === userName);
+
+    if (member) {
+      return of(member);
+    }
 
     return this.http.get<Member>(this.baseUrl + 'users/' + userName);
   }
