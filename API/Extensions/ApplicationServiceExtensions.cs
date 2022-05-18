9aae38eb0cea5928a73f635299d8dd2a439630a2
zakariabijoy <zakaria.bijoy@live.com>
2022-05-18 10:24:05 +0600

    Adding a like repository --api

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -20,6 +20,7 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
 
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
 
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLIke(int sourceUserId, int likedUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+
+    }
+}
