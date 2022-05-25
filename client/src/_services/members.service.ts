6e316d38c8af5a51d6692707d54ca3d251a57e55
zakariabijoy <zakaria.bijoy@live.com>
2022-05-25 11:22:33 +0600

    setting up the like functionality in the angular and do some modification in error interceptor for 400 error --client

--- client/src/_services/members.service.ts
+++ client/src/_services/members.service.ts
@@ -97,6 +97,14 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(userName: string) {
+    return this.http.post(this.baseUrl + 'likes/' + userName, {});
+  }
+
+  getLikes(predicate: string) {
+    return this.http.get(this.baseUrl + 'likes?=' + predicate);
+  }
+
   private getPaginatedResult<T>(url, params: HttpParams) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http.get<T>(url, { observe: 'response', params }).pipe(
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,16 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/_services/members.service';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
 })
 export class MemberCardComponent implements OnInit {
- @Input() member: Member
-  constructor() { }
+  @Input() member: Member;
 
-  ngOnInit(): void {
+  constructor(
+    private memberService: MembersService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {}
+
+  addLike(member: Member) {
+    this.memberService.addLike(member.userName).subscribe(() => {
+      this.toastr.success('You have liked ' + member.knownAs);
+    });
   }
-
 }
