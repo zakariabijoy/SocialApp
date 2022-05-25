021fd61f7b37e16a3f90e4bb91fe9ff5275c3f6d
zakariabijoy <zakaria.bijoy@live.com>
2022-05-25 11:43:07 +0600

    showing likes  in the list component --client

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MembersService } from 'src/_services/members.service';
+import { Member } from '../_models/member';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+  members: Partial<Member[]>;
+  predicate = 'liked';
 
-  constructor() { }
+  constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
+    this.loadLikes();
   }
 
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe((res) => {
+      this.members = res;
+    });
+  }
 }
