7febf656e3906111ef55b669b289b744159157d1
zakariabijoy <zakaria.bijoy@live.com>
2022-05-25 15:54:21 +0600

    paginating the likes on the client --client

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MembersService } from 'src/_services/members.service';
 import { Member } from '../_models/member';
+import { Pagination } from '../_models/pagination';
 
 @Component({
   selector: 'app-lists',
@@ -10,6 +11,9 @@ import { Member } from '../_models/member';
 export class ListsComponent implements OnInit {
   members: Partial<Member[]>;
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
+  pagination: Pagination;
 
   constructor(private memberService: MembersService) {}
 
@@ -18,8 +22,16 @@ export class ListsComponent implements OnInit {
   }
 
   loadLikes() {
-    this.memberService.getLikes(this.predicate).subscribe((res) => {
-      this.members = res;
-    });
+    this.memberService
+      .getLikes(this.predicate, this.pageNumber, this.pageSize)
+      .subscribe((res) => {
+        this.members = res.result;
+        this.pagination = res.pagination;
+      });
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadLikes();
   }
 }
