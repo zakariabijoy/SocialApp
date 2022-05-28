2e5b261090aeb35470357d6ad60b045ff71c0d11
zakariabijoy <zakaria.bijoy@live.com>
2022-05-28 19:12:21 +0600

    setting up the entities for messaging

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -5,24 +5,25 @@ namespace API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext( DbContextOptions options) : base(options)
+        public DataContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
             builder.Entity<UserLike>()
-                .HasKey(k => new {k.SourceUserId, k.LikedUserId});
-            
+                .HasKey(k => new { k.SourceUserId, k.LikedUserId });
+
             builder.Entity<UserLike>()
                 .HasOne(s => s.SourceUser)
                 .WithMany(l => l.LikedUsers)
-                .HasForeignKey( s => s.SourceUserId)
+                .HasForeignKey(s => s.SourceUserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
             builder.Entity<UserLike>()
@@ -30,6 +31,16 @@ namespace API.Data
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.LikedUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesRecevied)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -16,25 +12,25 @@ namespace API
     {
         public static async Task Main(string[] args)
         {
-           var host = CreateHostBuilder(args).Build();
-           using var scope = host.Services.CreateScope();
-           var services =  scope.ServiceProvider;
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
 
-           try
-           {
-               var context = services.GetRequiredService<DataContext>();
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
 
-               await context.Database.MigrateAsync();
-               await Seed.SeedUsers(context);
-           }
-           catch (System.Exception ex)
-           {
-               var logger = services.GetRequiredService<ILogger<Program>>();
-               logger.LogError(ex, "An error occured during migration");
-            
-           }
+                await context.Database.MigrateAsync();
+                await Seed.SeedUsers(context);
+            }
+            catch (System.Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
 
-          await host.RunAsync();
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
