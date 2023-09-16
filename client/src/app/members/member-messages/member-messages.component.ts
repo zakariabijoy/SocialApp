5d294d33facc35bc564bc4026fbbd82dd1713da3
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-17 00:53:28 +0600

    Adding the messaging feature: Activating the message tab

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -8,19 +8,10 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-  @Input() username?: string;
-  messages: Message[] = [];
+  @Input() messages: Message[] = [];
 
-  constructor(private messageService: MessageService) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  loadMessages(){
-    if(this.username){
-      this.messageService.getMessageThread(this.username).subscribe({
-        next: messages => this.messages = messages
-      })
-    }
-  }
+  } 
 }
