3be2b44cfda227175c068eb12c13bede41c919a8
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-17 03:13:41 +0600

    ASPNETCOre Identity and role management: Setting up the entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,14 +31,8 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
-  
-                user.UserName = registerDto.Username.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-                user.PasswordSalt = hmac.Key;
+            user.UserName = registerDto.Username.ToLower();
            
-
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
@@ -58,15 +52,6 @@ namespace API.Controllers
             var user = await _context.Users.Include(u => u.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
 
             if (user == null) return Unauthorized("Invalid UserName");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
-            }
 
             return new UserDto
             {
