1353fb3a582f869f70262222da13f59334ede8cb
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-21 01:18:50 +0600

    ASPNETCOre Identity and role management: Refactoring and adding a new migartion

--- API/Program.cs
+++ API/Program.cs
@@ -21,7 +21,7 @@ namespace API
                 var context = services.GetRequiredService<DataContext>();
 
                 await context.Database.MigrateAsync();
-                await Seed.SeedUsers(context);
+                //await Seed.SeedUsers(context);
             }
             catch (System.Exception ex)
             {
