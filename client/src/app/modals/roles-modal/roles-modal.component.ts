45aea80a3467fa6d7cc1cc294a12938c85a1084c
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-22 02:08:59 +0600

    ASPNETCore Identity and role management: Setting up modals

---
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-roles-modal',
+  templateUrl: './roles-modal.component.html',
+  styleUrls: ['./roles-modal.component.css']
+})
+export class RolesModalComponent implements OnInit {
+
+  title: string;
+  closeBtnName: string;
+  list: any[] = [];
+ 
+  constructor(public bsModalRef: BsModalRef) {}
+ 
+  ngOnInit() {
+    this.list.push('PROFIT!!!');
+  }
+
+}
